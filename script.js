9a239a5ed6830f94e1fa8c7ac4f8236262083bf9
Ghostified <bransonallan@gmail.com>
2024-10-16 23:45:40 +0300

    26:49

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+var menuBtn = document.getElementById("menuBtn")
+var sideNav = document.getElementById("sideNav")
+var menu = document.getElementById("menu")
 
+sideNav.style.right = "-250px"
+
+menuBtn.onclick = function(){
+  if (sideNav.style.right == "-250px"){
+    sideNav.style.right = "0";
+    menu.src = "images/x-regular-240.png";
+  } 
+  else {
+    sideNav.style.right = "-250px";
+    menu.src = "images/menu-regular-240.png";
+  }
+  
+}
